0cad895e7764e8c07b58827e97dce0f6226678c0
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2017-09-04 15:49:59 +0100

    resized the stop button, added constraints and wired up all buttons with PlaySoundsViewController Lesson 5 Video 1 to 9

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -12,6 +12,23 @@ class PlaySoundsViewController: UIViewController {
     
     var recordedAudioURL: URL!
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBAction func playSoundForButton(_ sender: Any) {
+        print("play sound")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        print("stop playing sound")
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- PitchPerfect/recordSoundsViewController.swift
+++ PitchPerfect/recordSoundsViewController.swift
@@ -71,3 +71,4 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
 }
 
+}
